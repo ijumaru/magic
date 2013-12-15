5ac1c6e753035bd9d99eeba2659e61613da3a428
nr <cocoapebles@gmail.com>
2013-12-15 11:45:18 +0900

    delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once("DraftFileReader.php");
-$dir = "C:\Users\\nr-mc\Documents\Games\Magic The Gathering Online\Drafts";
-$dfr = new DraftFileReader($dir."\ult-6467181-12-14-2013.txt");
-$dfr->read();
-var_dump($dfr);
